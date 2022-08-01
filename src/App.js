311db50157a7a7756f208950d2cf7843d10a0910
Atanas Vasilev <my.atanas.vasilev@gmail.com>
2022-08-01 08:13:05 +0300

    moved .mp3 files to /sounds

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ function App() {
   const sounds = {
     pingu: {
       name: "Pingu",
-      src: require("./noot.mp3"),
+      src: require("./sounds/noot.mp3"),
     },
     badumtss: {
       name: "BadumTss",
-      src: require("./badumtss.mp3"),
+      src: require("./sounds/badumtss.mp3"),
     },
     iphone: {
       name: "iPhoneRing",
-      src: require("./iPhoneRing.mp3"),
+      src: require("./sounds/iPhoneRing.mp3"),
     },
   };
 
